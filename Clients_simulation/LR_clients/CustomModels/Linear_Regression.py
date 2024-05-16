5732cb5f21f1b7c2d45461f3cdf4607053eb10d0
Yashvir Singh <yashveernathawat@gmail.com>
2024-05-16 22:49:08 +0530

    LR Client Added

--- Clients_simulation/LR_clients/CustomModels/Linear_Regression.py
+++ Clients_simulation/LR_clients/CustomModels/Linear_Regression.py
@@ -1,91 +1,46 @@
 import numpy as np
 
-"""
-    (i) This implementation of the Support Vector Machine (SVM) algorithm only classifies linear separable data nicely,
-     the kernel trick has not been implemented...because in the federated setting we can't share the support vectors to the server, 
-        which are the key to the kernel trick...although the kernel trick can be implemented 
-        in a secure multi-party computation setting but this problem has some other obvious challenges.
-"""
-
-
 class LinearRegression:
-    def __init__(self, C=1.0, gamma='auto', lr=0.01, n_iters=100):
-        self.C = C
-        self.gamma = gamma
-        self.weights = None
-        self.biases = None
+    def __init__(self , lr=0.01, n_iters=1):
         self.lr = lr
         self.n_iters = n_iters
-        self.is_binary = False
+        self.m = []
+        self.c = []
 
-    def fit_binary(self, X, y):
-        self.is_binary = True
-        n_samples, n_features = X.shape
-        weights = np.zeros(n_features)
-        bias = 0
-        lr = self.lr
-        n_iters = self.n_iters
-        binary_y = np.where(y == 1, 1, -1)
-        for epoch in range(n_iters):
-            for idx, x in enumerate(X):
-                decision = np.dot(x, weights) + bias
-                if binary_y[idx] * decision < 1:
-                    weights += lr * (binary_y[idx] * x - 2 * self.C * weights)
-                    bias += lr * binary_y[idx]
-                else:
-                    weights += lr * (-2 * self.C * weights)
-        self.weights = weights
-        self.biases = bias
+    def fit(self, X_train, Y_train):
+        # Ensure X_train and Y_train are numpy arrays
+        X_train = np.array(X_train)
+        Y_train = np.array(Y_train)
 
-    def fit(self, X, y):
-        n_samples, n_features = X.shape
-        n_classes = len(np.unique(y))
-        if n_classes == 2:
-            self.fit_binary(X, y)
-            return
+        # Get the number of samples
+        num_samples = float(len(X_train))
+        
+        # Gradient Descent
+        for i in range(self.n_iters):
+            Y_pred = np.dot(X_train, self.m) + self.c
+            residuals = Y_train - Y_pred
+            # print("m :",self.m[:5])
+            # print(X_train.shape,residuals.shape)
+            # print(np.dot(X_train.T,residuals))
+            #print("c" , self.c[:5])
+            #print("Check kar : ",X_train[:5])
+            D_m = (-2 / num_samples) * np.dot(X_train.T, residuals)
+            D_c = (-2 / num_samples) * np.sum(residuals)
+            self.m = self.m - self.lr * D_m
+            self.c = self.c - self.lr * D_c
 
-        if self.weights is None or self.biases is None:
-            self.weights = np.zeros((n_classes, n_features))
-            self.biases = np.zeros(n_classes)
-        # print("weights shape", self.weights.shape)
-        for i in range(n_classes):
-            binary_y = np.where(y == i, 1, -1)
-            weights = self.weights[i]
-            bias = self.biases[i]
-            lr = self.lr  # Learning rate
-            n_iters = self.n_iters  # Number of n_iters
-            for epoch in range(n_iters):
-                for idx, x in enumerate(X):
-                    decision = np.dot(x, weights) + bias
-                    if binary_y[idx] * decision < 1:
-                        weights += lr * (binary_y[idx] * x - 2 * self.C * weights)
-                        bias += lr * binary_y[idx]
-                    else:
-                        weights += lr * (-2 * self.C * weights)
-            self.weights[i] = weights
-            self.biases[i] = bias
+        print(f"Trained model parameters: m = {self.m[:5]}, c = {self.c[:5]}")
 
     def predict(self, X):
-        if self.weights is None or self.biases is None:
-            raise ValueError("Model has not been trained yet.")
-        decision_values = np.dot(X, self.weights.T) + self.biases
-
-        if self.is_binary:
-            return np.where(decision_values < 0, 0, 1)
-        return np.argmax(decision_values, axis=1)
-
-    def get_weights(self):
-        if self.weights is None:
-            raise ValueError("Model has not been trained yet.")
-        return self.weights
-
-    def get_biases(self):
-        if self.biases is None:
-            raise ValueError("Model has not been trained yet.")
-        return self.biases
-
-    def update_weights(self, new_weights):
-        self.weights = new_weights
-
-    def update_biases(self, new_biases):
-        self.biases = new_biases
+        X = np.array(X)
+        
+        pred_test = np.dot(X,self.m) + self.c
+        return pred_test
+    
+    def update_parameters(self,parameters_dict):
+        self.m = parameters_dict['m']
+        self.c = parameters_dict['c']
+    
+    def get_parameters(self):
+        local_parameter = {'m' : self.m.tolist() , 'c' : self.c.tolist()}
+        return local_parameter
--- Server/Server.py
+++ Server/Server.py
@@ -7,6 +7,7 @@ weight_aggregated_time = time.time()
 num_clients = 1  # Assuming there are 5 clients
 num_rounds = 3
 
+
 class Server:
     def __init__(self,globals_parameters, max_round):
         self.globals_parameters = globals_parameters
@@ -37,21 +38,28 @@ class Server:
 
 
 
-async def send_request(session, url, data, client_id):
+async def send_request(session, url, data, client_id,server):
     async with session.post(url, json=data) as response:
         print(f"Response from client {client_id}: {response.status}")
-        return await response.json()  # Return the JSON response
+        response_data = await response.json()  # Return the JSON response
+        
+        client_id = response_data['client_id']
+        client_parameters = response_data['parameters']
+
+        server.update_queue[client_id] = client_parameters
+
+
+
 
 async def send_updated_parameters_to_clients(server):
-    updated_parameters = {"parameters" : server.globals_parameters}
+    updated_parameters = {"parameters" : server.globals_parameters,"round_num":server.curr_round}
     async with aiohttp.ClientSession() as session:
         tasks = []
         for client_id in range(1, num_clients + 1):
             client_url = f"http://localhost:500{client_id}/receive_parameters_and_run_model"
-            task = asyncio.create_task(send_request(session, client_url, updated_parameters, client_id))
+            task = asyncio.create_task(send_request(session, client_url, updated_parameters, client_id,server))
             tasks.append(task)
-        responses = await asyncio.gather(*tasks)  # Gather responses
-        return responses  # Return all responses
+        await asyncio.gather(*tasks)  # Gather responses
 
 
 
@@ -59,9 +67,17 @@ async def send_updated_parameters_to_clients(server):
 # Modify the __main__ block accordingly to use the responses
 if __name__ == '__main__':
     
-    globals_parameters = {"w":[0], "b":[0]}
-    max_round = 3
-    server = Server(globals_parameters,max_round)
+    
+    # Define the size of arrays
+    m_size = 22
+    c_size = 1
+
+    # Initialize the global parameters dictionary
+    global_parameters = {"m": [0 for i in range(m_size)], "c": [0 for i in range(c_size)]}
+
+
+    max_round = 1
+    server = Server(global_parameters,max_round)
 
     start_learning = input("Press 'Y' to start Federated Learning: ")
     if start_learning.upper() == 'Y':
@@ -71,6 +87,6 @@ if __name__ == '__main__':
             print("-"*50)
             loop = asyncio.get_event_loop()
             responses = loop.run_until_complete(send_updated_parameters_to_clients(server))
-            print("Responses from clients:", responses)
+            #print("Responses from clients:", responses)
     else:
         print("Federated Learning not started.")
